a3134b9b2c4621bf91f9077f7d7ef3c7ad434a5c
siddhanth36 <sksiddhanth69@gmail.com>
2026-02-22 16:12:12 +0530

    fix the portfolio v12

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 
 import portfolioRoute from "./routes/portfolio.js";
+import blogsRoute from "./routes/blogs.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,10 +12,11 @@ app.use(cors({ origin: true }));
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
-  res.send("SERVER RUNNING - STEP 2");
+  res.send("SERVER RUNNING - STEP 3");
 });
 
 app.use("/api/portfolio", portfolioRoute);
+app.use("/api/blogs", blogsRoute);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("SERVER STARTED ON PORT", PORT);
